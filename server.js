58e22a4e9edd0f653115e7ebbc13a5f4e689b13c
EmilyBernard <bernam2000@hotmail.com>
2023-03-20 16:15:26 -0400

    debug6

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
-
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3000;
 
 
 app.use(express.urlencoded({ extended: true }));
@@ -10,7 +9,7 @@ app.use(express.static('public'));
 const routerapi = require('./routes/apiRoutes')(app);
 const routerhtml = require('./routes/htmlRoutes')(app);
 
-app.listen(PORT, () =>
+app.listen(PORT, function() =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
 
