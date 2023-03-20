019484bd44ec9907a2dcb88f7a8e86821ef07e76
Emily Bernard <112719573+EmilyBernard@users.noreply.github.com>
2023-03-20 16:29:21 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,8 +6,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-const routerapi = require('./routes/apiRoutes')(app);
-const routerhtml = require('./routes/htmlRoutes')(app);
+
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
