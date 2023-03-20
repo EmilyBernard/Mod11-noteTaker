16f2656382382ceb5611c71da421648c214b9eca
Emily Bernard <112719573+EmilyBernard@users.noreply.github.com>
2023-03-20 16:46:03 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const PORT = process.env.PORT || 3000;
 const app = express();
 
+const routerapi = require('./routes/apiRoutes')(app);
+const routerhtml = require('./routes/htmlRoutes')(app);
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
