484a56cab148716b245a5f3448320d10694103c9
EmilyBernard <bernam2000@hotmail.com>
2023-03-20 16:13:04 -0400

    debug5

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
-app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-//const routerapi = require('./routes/apiRoutes')(app);
-//const routerhtml = require('./routes/htmlRoutes')(app);
+app.use(express.static('public'));
+const routerapi = require('./routes/apiRoutes')(app);
+const routerhtml = require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
