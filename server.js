b8fbce88508c66f480a1abaaf27f1cb7ca18612f
EmilyBernard <bernam2000@hotmail.com>
2023-03-21 17:38:07 -0400

    refactor routes

--- server.js
+++ server.js
@@ -13,7 +13,16 @@ app.use(express.json());
 app.use(express.static('public'));
 
 const routerapi = require('./routes/apiRoutes')(app);
-const routerhtml = require('./routes/htmlRoutes')(app);
+
+// GET route for notes page
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+  });
+  
+// GET route for homepage
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+  });
 
 app.listen(PORT, function() {
     console.log(`App listening on http://localhost:${PORT}`);
